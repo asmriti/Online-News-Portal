4d60742183ec63288a9907caa2fe0a85466a1887
Smriti Aryal <smritiaryal331@gmail.com>
2022-12-12 18:23:00 +0545

    Improvised design

---
+++ src/components/CommunitySection.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function CommunitySection() {
+  return (
+    <div className="container mx-auto mt-[3rem]">
+      <h1 className="text-2xl font-bold text-center">Community Section</h1>
+      <ul className="p-6 bg-white border-2 border-transparent rounded-lg mt-[20px]">
+        <li></li>
+        <li>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </li>
+
+        <li>
+          <button>Join Now</button>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default CommunitySection;
